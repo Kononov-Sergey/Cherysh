d767ef4d319e896d7023afff097154a32dc30cfc
Sergei Kononov <kobo220376@gmail.com>
2022-04-03 12:36:13 +0300

    fixed minor issues

--- js/script.js
+++ js/script.js
@@ -1,34 +1,34 @@
 $(document).ready(function () {
-   // burger
-   $('.nav__burger').click(function (event) {
-      $('.nav__burger,.header__menu,.nav_boot').toggleClass('active');
-      $('.body').toggleClass('lock')
-   });
-   // ==================================================================
+  // burger
+  $(".nav__burger").click(function (event) {
+    $(".nav__burger,.header__menu,.nav_boot").toggleClass("active");
+    $(".body").toggleClass("lock");
+  });
+  // ==================================================================
 
-   // scroll to #
-   $('header a').click(function () {
-      let element = $(this).attr('href');
-      const distant = $(element).offset().top - $('.navBar').height();
-      $('html, body').stop().animate({ 'scrollTop': distant }, 800);
-      $('.nav__burger,.header__menu,.nav_boot').toggleClass('active');
-   });
-   // ================================================================== 
+  // scroll to #
+  $("header a").click(function () {
+    let element = $(this).attr("href");
+    const distant = $(element).offset().top - $(".navBar").height();
+    $("html, body").stop().animate({ scrollTop: distant }, 800);
+    $(".nav__burger,.header__menu,.nav_boot").toggleClass("active");
+  });
+  // ==================================================================
 
-   // button up
-   let cont_dist = $('.contacts').offset().top - $(window).height();
-   console.log(cont_dist)
-   $(window).scroll(function () {
-      if ($(this).scrollTop() > 300 && $(this).scrollTop() < cont_dist) {
-         $('.button').addClass('show');
-      } else {
-         $('.button').removeClass('show');
-      }
-   });
+  // button up
+  let cont_dist = $(".contacts").offset().top - $(window).height();
 
-   $('.button').click(function () {
-      $('html, body').animate({ scrollTop: 0 }, 800);
-      return false;
-   });
-   // ==================================================================
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 300 && $(this).scrollTop() < cont_dist) {
+      $(".button").addClass("show");
+    } else {
+      $(".button").removeClass("show");
+    }
+  });
+
+  $(".button").click(function () {
+    $("html, body").animate({ scrollTop: 0 }, 600);
+    return false;
+  });
+  // ==================================================================
 });
